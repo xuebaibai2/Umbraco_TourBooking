7bdcb74a015d7f55bd8934d266ae8dc1a0b7dd0a
Cayden <cayden.li@thankq.com.au>
2016-11-28 17:09:56 +1100

    Update Master Page

---
+++ SYJMA.Umbraco/Scripts/emailSubscription.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $("#sub_btn").click(function () {
+        var name = $("#sub_name").val();
+        var email = $("#sub_email").val();
+        alert('Name: ' + name + '\r' + 'Email: ' + email);
+    });
+})
