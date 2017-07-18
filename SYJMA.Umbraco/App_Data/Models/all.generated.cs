0a6d41e7cff692cbc312875c376ae79c89924d8c
Cayden <caydenli@outlook.com>
2017-07-18 15:43:28 +1000

    Commit before perform calendar change

--- SYJMA.Umbraco/App_Data/Models/all.generated.cs
+++ SYJMA.Umbraco/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "3f61851ecded9968")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
